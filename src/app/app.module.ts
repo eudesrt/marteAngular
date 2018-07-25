606c7237f626ddd5928e640f72c4408787ddb20c
DESKTOP-F52P6MD\Rafael <eudesrt@gmail.com>
2018-07-25 16:19:45 -0300

    Novo projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {InputTextModule} from 'primeng/components/inputtext/inputtext';
+
+import {ButtonModule} from 'primeng/components/button/button';
+
+import {DataTableModule} from 'primeng/components/datatable/datatable';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    InputTextModule,
+    ButtonModule,
+    BrowserModule,
+    DataTableModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
