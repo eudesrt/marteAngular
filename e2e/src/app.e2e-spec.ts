e52346a94a71b850ad058692c5c8caedc3713c25
DESKTOP-F52P6MD\Rafael <eudesrt@gmail.com>
2018-07-26 12:06:57 -0300

    correção ambiente

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to marte-ui!');
+    expect(page.getParagraphText()).toEqual('Welcome to my-mateAngula-api!');
   });
 });
